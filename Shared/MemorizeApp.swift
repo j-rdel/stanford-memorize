53536ab5623cdaa0657d7def141105a5915c6cbd
Jardel Urban <jardelurban3@gmail.com>
2022-04-19 01:39:09 -0300

    Initial Commit

---
+++ Shared/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Shared
+//
+//  Created by Jardel Urban on 19/04/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
